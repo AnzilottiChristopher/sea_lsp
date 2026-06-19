6527f5018a16e573ad1cc65782c338639645129d
Chris Anzilotti <anzilottichr@gmail.com>
2026-06-18 23:57:48 -0400

    Collects ClassInfo

--- src/indexer.rs
+++ src/indexer.rs
@@ -1,6 +1,6 @@
-use crate::symbol_table::{self, SymbolTable};
+use crate::symbol_table::{ClassInfo, FieldInfo, MethodInfo, SymbolTable};
 use std::path::PathBuf;
-use tree_sitter::{Parser, Tree};
+use tree_sitter::{Node, Parser, Tree};
 use walkdir::WalkDir;
 
 fn collect_files() -> Vec<PathBuf> {
@@ -38,6 +38,164 @@ fn parse_dirs(dir: &Vec<PathBuf>) -> Vec<(Tree, String)> {
     sources
 }
 
-fn collect_class_info(tree: Tree, source: &String) {
-    let mut info: SymbolTable;
+fn collect_info(tree: Tree, source: &String, symbol_table: &mut SymbolTable, file: &PathBuf) {
+    let root = tree.root_node();
+    let mut cursor = root.walk();
+
+    for child in root.children(&mut cursor) {
+        match child.kind() {
+            "import_declaration" => {
+                //TODO look into imports methods for lsp
+                //Needs to recursively call this function to find them
+            }
+            "class_declaration" => {
+                // First grab basic class info like name
+                // Then walk into class for pub fields and methods
+                let mut class_info: ClassInfo = ClassInfo::default();
+                collect_class_info(&child, source, &mut class_info);
+            }
+            _ => {}
+        }
+    }
+}
+
+fn collect_class_info(node: &Node, source: &String, class_info: &mut ClassInfo) {
+    let name_node = node.child_by_field_name("name").unwrap();
+
+    //Name and line fields for class info
+    let name = source[name_node.start_byte()..name_node.end_byte()].to_string();
+    let line = node.start_position().row + 1;
+
+    //inherit and implements if it has
+    let inherits = node
+        .child_by_field_name("inherit")
+        .and_then(|inherit| inherit.child_by_field_name("parent"))
+        .map(|p| source[p.start_byte()..p.end_byte()].to_string());
+
+    let mut implements = Vec::new();
+    if let Some(implements_node) = node.child_by_field_name("implements") {
+        let mut cursor = implements_node.walk();
+        for child in implements_node.children(&mut cursor) {
+            if child.kind() == "identifier" {
+                implements.push(source[child.start_byte()..child.end_byte()].to_string());
+            }
+        }
+    }
+
+    // Now need to walk through the body using for loop
+    let mut cursor = node.walk();
+
+    for child in node.children(&mut cursor) {
+        match child.kind() {
+            "field_declaration" => {
+                class_info.fields.push(collect_field_info(&child, source));
+            }
+            "method_declaration" => {
+                class_info.methods.push(collect_method_info(&child, source));
+            }
+            "constructor_declaration" => {
+                let con_name_node = child.child_by_field_name("name").unwrap();
+                let con_name =
+                    source[con_name_node.start_byte()..con_name_node.end_byte()].to_string();
+                let mut method = collect_method_info(&child, source);
+                method.is_constructor = con_name == class_info.name;
+                class_info.methods.push(method);
+            }
+            "drop_declaration" => {
+                class_info.methods.push(MethodInfo {
+                    name: "drop".to_string(),
+                    params: Vec::new(),
+                    return_type: "void".to_string(),
+                    line: child.start_position().row + 1,
+                    is_pub: child.child_by_field_name("visibility").is_some(),
+                    is_constructor: false,
+                    is_drop: true,
+                });
+            }
+            _ => {}
+        }
+    }
+
+    class_info.name = name;
+    class_info.parent = inherits;
+    class_info.implements = implements;
+    class_info.line = line;
+}
+
+fn collect_field_info(node: &Node, source: &String) -> FieldInfo {
+    let is_pub = node.child_by_field_name("visibility").is_some();
+
+    let type_node = node.child_by_field_name("type").unwrap();
+    let base_node = type_node.child_by_field_name("base").unwrap();
+    let type_text = source[base_node.start_byte()..base_node.end_byte()].to_string();
+
+    let name_node = node.child_by_field_name("name").unwrap();
+    let name = source[name_node.start_byte()..name_node.end_byte()].to_string();
+
+    let line = node.start_position().row + 1;
+
+    FieldInfo {
+        name,
+        type_name: type_text,
+        line,
+        is_pub,
+    }
+}
+fn collect_method_info(node: &Node, source: &String) -> MethodInfo {
+    // method_declaration wraps sea_style_method or c_style_method
+    let method_node = node.child(0).unwrap();
+
+    // check if pub
+    let is_pub = method_node.child_by_field_name("visibility").is_some();
+
+    // get name
+    let name_node = method_node.child_by_field_name("name").unwrap();
+    let name = source[name_node.start_byte()..name_node.end_byte()].to_string();
+
+    // get return type
+    let return_type = match method_node.child_by_field_name("return_type") {
+        Some(return_node) => match return_node.named_child(0) {
+            Some(type_node) => match type_node.child_by_field_name("base") {
+                Some(base_node) => source[base_node.start_byte()..base_node.end_byte()].to_string(),
+                None => "void".to_string(),
+            },
+            None => "void".to_string(),
+        },
+        None => "void".to_string(),
+    };
+
+    // get params
+    let params = match method_node.child_by_field_name("parameters") {
+        Some(params_node) => {
+            let mut params = Vec::new();
+            let mut cursor = params_node.walk();
+            for param in params_node.children(&mut cursor) {
+                if param.kind() == "sea_parameter" {
+                    let type_node = param.child_by_field_name("type").unwrap();
+                    let base_node = type_node.child_by_field_name("base").unwrap();
+                    let type_text =
+                        source[base_node.start_byte()..base_node.end_byte()].to_string();
+                    let name_node = param.child_by_field_name("name").unwrap();
+                    let name_text =
+                        source[name_node.start_byte()..name_node.end_byte()].to_string();
+                    params.push((type_text, name_text));
+                }
+            }
+            params
+        }
+        None => Vec::new(),
+    };
+
+    // get line
+    let line = node.start_position().row + 1;
+
+    MethodInfo {
+        name,
+        params,
+        return_type,
+        line,
+        is_pub,
+        is_constructor: false,
+        is_drop: false,
+    }
 }
--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -1,29 +1,34 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
 
+#[derive(Default)]
 pub struct ClassInfo {
-    name: String,
-    fields: Vec<FieldInfo>,   // pub fields only
-    methods: Vec<MethodInfo>, // pub methods only
-    parent: Option<String>,
-    implements: Vec<String>,
-    file: PathBuf,
-    line: usize,
+    pub name: String,
+    pub fields: Vec<FieldInfo>,   // pub fields only
+    pub methods: Vec<MethodInfo>, // pub methods only
+    pub parent: Option<String>,
+    pub implements: Vec<String>,
+    pub file: PathBuf,
+    pub line: usize,
 }
 
 pub struct FieldInfo {
-    name: String,
-    type_name: String,
-    line: usize,
+    pub name: String,
+    pub type_name: String,
+    pub line: usize,
+    pub is_pub: bool,
 }
 
 pub struct MethodInfo {
-    name: String,
-    params: Vec<(String, String)>, // (type, name)
-    return_type: String,
-    line: usize,
+    pub name: String,
+    pub params: Vec<(String, String)>, // (type, name)
+    pub return_type: String,
+    pub line: usize,
+    pub is_pub: bool,
+    pub is_constructor: bool,
+    pub is_drop: bool, // Should of made the three is_* an enum
 }
 
 pub struct SymbolTable {
-    classes: HashMap<String, ClassInfo>,
+    pub classes: HashMap<String, ClassInfo>,
 }
